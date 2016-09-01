cbd819233a00ad5fc507a034463645cccc31a1c7
hiromaily <hiromaily2@gmail.com>
2016-09-01 17:54:35 +0900

    added kafka settings

--- projects/01_messaging.sh
+++ projects/01_messaging.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Environment Variable
-RMQ_FLG=1
+RMQ_FLG=0
+KAFKA_FLG=1
 
 ###############################################################################
 # RabbitMQ
@@ -63,6 +64,50 @@ docker ps -a
 # Apache Kafka
 ###############################################################################
 #https://hub.docker.com/r/wurstmeister/kafka/
+#http://wurstmeister.github.io/kafka-docker/
+#https://github.com/wurstmeister/kafka-docker
 
+#git clone https://github.com/wurstmeister/kafka-docker.git
 
+if [ $KAFKA_FLG -eq 1 ]; then
 
+    # Environment
+    CLONE_BRANCH=0
+    GITREPO=https://github.com/wurstmeister/kafka-docker.git
+    GITDIR=${PWD}/kafka/kafka-docker
+    MY_IP_ADDRESS=100.69.18.116
+
+    #
+    docker rm -f kafkadocker_kafka_1
+    docker rm -f kafkadocker_zookeeper_1
+
+    # Git
+    if [ $CLONE_BRANCH -eq 1 ]; then
+        rm -rf ${GITDIR}
+        pushd ./kafka/
+        git clone ${GITREPO}
+        EXIT_STATUS=$?
+
+        if [ $EXIT_STATUS -gt 0 ]; then
+            exit $EXIT_STATUS
+        fi
+
+        popd
+    fi
+
+    # change docker-compose.yml
+    #KAFKA_ADVERTISED_HOST_NAME: ${MY_IP_ADDRESS}
+
+    # Run by docker-compose
+    pushd ${GITDIR}
+    docker-compose up -d
+
+    # stop
+    #docker-compose stop
+
+    # kafkadocker_kafka
+    #0.0.0.0:32768->9092/tcp
+
+    # wurstmeister/zookeeper
+    #22/tcp, 2888/tcp, 3888/tcp, 0.0.0.0:2181->2181/tcp
+fi
